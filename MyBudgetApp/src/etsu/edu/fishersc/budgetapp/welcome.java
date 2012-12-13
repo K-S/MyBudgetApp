10c3d5c0c483e68794c064eb116718800558ad5f
fishersc <geeksquad507@gmail.com>
2012-12-13 11:53:30 -0500

    Revert "changed weclome screen to not show income category"
    This reverts commit d1f54b0194883fbcf19aac27538f2c6893f6831a.

--- MyBudgetApp/src/etsu/edu/fishersc/budgetapp/welcome.java
+++ MyBudgetApp/src/etsu/edu/fishersc/budgetapp/welcome.java
@@ -47,6 +47,12 @@ public class welcome extends Activity
         act = this;
     	newScrollView = (ScrollView) findViewById(R.id.categoryScrollView);
     	
+    	///
+    	///
+    	//set size of scroll view to be dynamic based on screen size so
+    	//bottom buttons are not off screen
+    	///
+    	///
     	DisplayMetrics metrics = getResources().getDisplayMetrics();
     	int y = metrics.heightPixels;
     	newScrollView.getLayoutParams().height = y /3;
@@ -83,7 +89,7 @@ public class welcome extends Activity
     	
     	
 
-    		for(int i = 1; i < categorys.size(); i++)
+    		for(int i = 0; i < categorys.size(); i++)
     		{
     		
     		
